07b60e617088ed687439e121b9ff6afa9eaf81ea
derlisGonzalez <freddyrube2013@gmail.com>
2021-04-09 14:31:22 -0400

    09-04-21

---
+++ src/app/models/proyecto.model.ts
@@ -0,0 +1,14 @@
+
+export class ProyectoModel {
+
+    id: string;
+    codigo?: string;
+    titulo?: string;
+    cuerpo?: string;
+    autor?: string;
+    autor2?: string;
+    estado?: boolean;
+    criterios?: string;
+    
+
+}
